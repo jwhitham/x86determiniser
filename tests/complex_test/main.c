19558059f88e4f29b497fc8075cca81764ba0c84
Jack Whitham <jack.d.whitham@gmail.com>
2020-04-25 04:13:37 +0100

    Remove ELF-specific directives from reference assembly code and improve the rebuild process

--- tests/complex_test/main.c
+++ tests/complex_test/main.c
@@ -1,43 +1,16 @@
-#ifdef WIN32
-#include <windows.h>
-#define WIN
-#elif WIN64
-#include <windows.h>
-#define WIN
-#else
-#include <dlfcn.h>
-#endif
-
 #include <stdint.h>
 #include <stdio.h>
 
 
 void all_tests (void);
 
-int main(int argc, char ** argv)
+int main(void)
 {
+#ifdef USE_OLD_VERSION_TO_MAKE_A_REFERENCE_FILE
    uint32_t ER[2];
-   void (* startup_x86_determiniser) (uint32_t * ER) = NULL;
-
-   if (argc == 2) {
-#ifdef WIN
-      HMODULE lib = LoadLibrary (argv[1]);
-      if (!lib) {
-         return 1;
-      }
-      startup_x86_determiniser = (void *) GetProcAddress (lib, "startup_x86_determiniser");
-#else
-      void * lib = dlopen (argv[1], RTLD_NOW);
-      if (!lib) {
-         return 1;
-      }
-      startup_x86_determiniser = (void *) dlsym (lib, "startup_x86_determiniser");
+   void startup_x86_determiniser (uint32_t * ER);
+   startup_x86_determiniser (ER);
 #endif
-      if (!startup_x86_determiniser) {
-         return 2;
-      }
-      startup_x86_determiniser (ER);
-   }
 
    printf ("start\n");
    all_tests ();
